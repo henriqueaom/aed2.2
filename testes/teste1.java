f34e79b3d125a105c79c2e5d68221365e66bf142
SrNescau02 <henriqueaom02@gmail.com>
2024-07-24 13:25:43 -0300

    teste1

---
+++ testes/teste1.java
@@ -0,0 +1,14 @@
+package testes;
+
+/**
+ * teste1
+ */
+public class teste1 {
+
+    public static void main(String[] args) {
+        System.err.println("hello world");
+        
+    }
+
+    
+}
