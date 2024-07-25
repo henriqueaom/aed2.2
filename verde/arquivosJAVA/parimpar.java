34d9ddde28bc43f8fbb80cd52dd7b3331730a027
SrNescau02 <henriqueaom02@gmail.com>
2024-07-25 13:07:26 -0300

    par

--- verde/arquivosJAVA/parimpar.java
+++ verde/arquivosJAVA/parimpar.java
@@ -1,5 +1,41 @@
 package verde.arquivosJAVA;
 
+import java.util.*;
+
+
 public class parimpar {
+    
+
+    public static void parouimpar(int n){
+        if(n%2==0){
+            System.err.println("par");
+        }else{
+            System.err.println("impar");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        do{
+
+        int n = sc.nextInt();
+
+        if(n==0){
+            break;
+        }
+
+        parouimpar(n);
+
+      
+
+    }while(true);
+
+
+
+        sc.close();
+    }
     
 }
